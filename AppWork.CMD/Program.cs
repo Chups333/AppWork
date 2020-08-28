78b52d47c583df24e9f749def8799e13b7be462f
Petrov Sasha <sasha1327@mail.ru>
2020-08-28 19:32:27 +0800

    Добавил простую реализацию действия с браузером

--- AppWork.CMD/Program.cs
+++ AppWork.CMD/Program.cs
@@ -6,6 +6,9 @@ using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using System.Threading;
 
 namespace AppWork.CMD
 {
@@ -13,28 +16,30 @@ namespace AppWork.CMD
     {
         static void Main(string[] args)
         {
-
+            IWebDriver web;
             // чтобы дописать поля в таблицы нужно
             //в Меню-Вид-Другие окна-Консоль диспетчера пакетов написать строку enable-migrations и add-migration AddGroupType и после update-database
             // эта строка разрешает совершать миграции программно - дописать поле таблицы (без ошибки)
 
             Console.WriteLine("Добро пожаловать в приложение Robot!!!");
-           
+
 
             var logDateTimeOpen = DateTime.Now;
-            
+
             var robotLogsController = new RobotLogsController(logDateTimeOpen);
             if (robotLogsController.IsNew)
             {
                 var logTextOpen = "Открыл приложение";
 
-                robotLogsController.SetNewData(logTextOpen) ;
+                robotLogsController.SetNewData(logTextOpen);
             }
             while (true)
             {
                 Console.WriteLine();
                 Console.WriteLine("Что вы хотите сделать?");
                 Console.WriteLine("E - Вывести журнал");
+                Console.WriteLine("A - Работа с HPSM");
+
                 Console.WriteLine("Q - выход");
                 var key = Console.ReadKey();
                 Console.WriteLine();
@@ -42,17 +47,86 @@ namespace AppWork.CMD
                 {
                     case ConsoleKey.E:
                         var result = robotLogsController.RobotLogsList.OrderBy(p => p.LogDataTime);
-                        
+
                         foreach (var item in result)
                         {
                             Console.WriteLine();
                             Console.WriteLine($"{item.LogDataTime} - {item.LogText}");
                         }
                         break;
+                    case ConsoleKey.A:
+
+                        Console.WriteLine("Введите логин");
+                        var LOGIN = Console.ReadLine();
+                        Console.WriteLine("Введите пароль");
+                        var PASS = Console.ReadLine();
+
+
+                        web = new ChromeDriver();
+
+                        web.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(20);
+                        web.Navigate().GoToUrl("https://support.rosatom.ru/sm");
+                        web.FindElement(By.XPath("//input[@id='username']")).SendKeys(LOGIN);
+                        web.FindElement(By.XPath("//input[@id='password']")).SendKeys(PASS);
+                        web.FindElement(By.XPath("//input[@id='SubmitCreds']")).Click();
+
+                        #region CommentConnect;
+                        //try
+                        //{
+                        //    try
+                        //    {
+                        //        web.Navigate().GoToUrl("https://support.rosatom.ru/sm");
+                        //    }
+                        //    catch (Exception)
+                        //    {
+
+                        //        Console.WriteLine("Ошибка. Не удалось перейти по URL");
+                        //    }
+
+                        //    try
+                        //    {
+                        //        web.FindElement(By.XPath("//input[@id='username']")).SendKeys(LOGIN);
+                        //    }
+                        //    catch (Exception)
+                        //    {
+                        //        Console.WriteLine("Ошибка. Не удалось ввести логин");
+                        //    }
+
+                        //    try
+                        //    {
+                        //        web.FindElement(By.XPath("//input[@id='password']")).SendKeys(PASS);
+                        //    }
+                        //    catch (Exception)
+                        //    {
+                        //        Console.WriteLine("Ошибка. Не удалось ввести пароль");
+
+                        //    }
+                        //    try
+                        //    {
+                        //        web.FindElement(By.XPath("//input[@id='SubmitCreds']")).Click();
+                        //    }
+                        //    catch (Exception)
+                        //    {
+                        //        Console.WriteLine("Ошибка. Не удалось нажать на кнопку ВОЙТИ");
+
+                        //    }
+
+
+
+                        //}
+                        //catch (Exception)
+                        //{
+
+
+                        //}
+                        #endregion
+
+                        
+                        break;
                     case ConsoleKey.Q:
 
                         var logDateTimeClose = DateTime.Now;
-                        
+
                         robotLogsController = new RobotLogsController(logDateTimeClose);
 
                         if (robotLogsController.IsNew)
@@ -61,10 +135,14 @@ namespace AppWork.CMD
 
                             robotLogsController.SetNewData(logTextClose);
                         }
+                        
+                        //web.Quit();
                         Environment.Exit(0);
                         break;
                 }
             }
         }
+
+
     }
 }
