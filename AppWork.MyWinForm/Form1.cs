746460ca126fb34ff1802129d8be6e9cbad25c18
Petrov Sasha <sasha1327@mail.ru>
2020-08-31 11:05:40 +0300

    Доработки Формы

--- AppWork.MyWinForm/Form1.cs
+++ AppWork.MyWinForm/Form1.cs
@@ -13,6 +13,8 @@ using System.Runtime.InteropServices;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium;
 using System.Threading;
+using OpenQA.Selenium.Interactions;
+using OpenQA.Selenium.Remote;
 
 namespace AppWork.MyWinForm
 {
@@ -25,7 +27,7 @@ namespace AppWork.MyWinForm
 
         private void Form1_Shown(object sender, EventArgs e)
         {
-           
+
             // чтобы дописать поля в таблицы нужно
             //в Меню-Вид-Другие окна-Консоль диспетчера пакетов написать строку enable-migrations и add-migration AddGroupType и после update-database
             // эта строка разрешает совершать миграции программно - дописать поле таблицы (без ошибки)
@@ -74,15 +76,37 @@ namespace AppWork.MyWinForm
             web.Navigate().GoToUrl("https://support.rosatom.ru/sm");
             web.Manage().Window.Maximize();
 
-            var findElement = web.FindElements(By.XPath("//span[@id='cwc_masthead_username']")).Count();
-            if (findElement == 0)
+            var findElementList = web.FindElements(By.XPath("//span[@id='cwc_masthead_username']"));
+            var findElement = findElementList.Count();
+            while (findElement == 0)
             {
                 web.FindElement(By.XPath("//input[@id='username']")).SendKeys(LOGINTEXT.Text);
                 web.FindElement(By.XPath("//input[@id='password']")).SendKeys(PASSTEXT.Text);
                 web.FindElement(By.XPath("//input[@id='SubmitCreds']")).Click();
+                findElement = 1;
             }
+
+
+            //var elements = web.FindElements(By.CssSelector("#ext-gen-list-0-17 table")).ToList();
+
+            //MAGAZINETEXT.AppendText(elements.Count().ToString());
+
+            Thread.Sleep(20000);
+            var search = web.FindElement(By.XPath("//em/button[@id='ext-gen-top365']")).Location;
+            var actiomClick = new Actions(web);
+            actiomClick.MoveByOffset(search.X,search.Y+240).Click();
+            actiomClick.Build().Perform();
+
+            //MAGAZINETEXT.AppendText(search.X.ToString()+" "+search.Y.ToString());
+            //var actiomClick = new Actions(web).Click(search);)
+            //actiomClick.Build().Perform();
+            //web.Quit();
+
+
+
+
         }
 
-        
+
     }
 }
